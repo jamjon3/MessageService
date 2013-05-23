a70881491c93c158bd117a6d65ee743ede5ae672
Eric Pierce <epierce@usf.edu>
2013-05-23 15:28:38 -0400

    dependency cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,8 +9,7 @@ grails.project.war.file = "target/${appName}.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
+        excludes 'ehcache'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
@@ -24,29 +23,18 @@ grails.project.dependency.resolution = {
         mavenCentral()
    }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-       // runtime 'mysql:mysql-connector-java:5.1.16'
+      compile "commons-lang:commons-lang:2.5"
     }
 
     plugins {
-        runtime ":resources:1.1.6"
-        compile "commons-lang:commons-lang:2.5"
-        
         compile ":mongodb:1.2.0"
-
-        test ":code-coverage:1.2.6"
-
         compile ":joda-time:1.4"
         compile ":perf4j:0.1.1"
         compile ":spring-security-core:1.2.7.3"
         compile ":spring-security-cas-usf:1.2.0"
         compile ":spring-security-ldap:1.0.6"
 
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.4"
+        test ":code-coverage:1.2.6"
 
         build ":tomcat:$grailsVersion"
     }
